4142d2bcf487166ec136ce143eb5f2e663911081
Yen-Hsun, Huang <s011208@gmail.com>
2014-07-03 01:35:57 +0800

    disable debug

--- code/src/com/bj4/yhh/accountant/activities/TestActivity.java
+++ code/src/com/bj4/yhh/accountant/activities/TestActivity.java
@@ -32,7 +32,7 @@ import android.widget.ViewSwitcher;
 
 public class TestActivity extends BaseActivity {
 
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     public static final String TAG = "QQQQ";
 
